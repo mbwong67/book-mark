040ba2646f9fefed6f5e06c9bdefcfb163c325e4
Patrick Cash <patrickcash@gmail.com>
2023-02-01 13:35:46 -0500

    fix for heroku

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import bookmarkRoutes from './routes/bookmarks.js';
 import userRoutes from './routes/users.js'
